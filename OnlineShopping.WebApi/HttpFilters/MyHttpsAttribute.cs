050fc8413fb90349ba34c22e3334446b4495d671
sasaniaidin <sasaniaidin@gmail.com>
2024-06-25 22:09:49 +0330

    Add http Filters

---
+++ OnlineShopping.WebApi/HttpFilters/MyHttpsAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopping.WebApi.HttpFilters
+{
+    public class MyHttpsAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!context.HttpContext.Request.IsHttps)
+            {
+                context.Result = new StatusCodeResult(NewStatusCodes.Status520Aidin_Sasani_1993);
+            }
+        }
+    }
+    {
+    }
+}
---
+++ OnlineShopping.WebApi/HttpFilters/NewStatusCodes.cs
@@ -0,0 +1,7 @@
+﻿namespace OnlineShopping.WebApi.HttpFilters
+{
+    public static class NewStatusCodes
+    {
+        public const int Status520Aidin_Sasani_1993 = 520;
+    }
+}
